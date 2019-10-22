019ebde04344905662a4503e2ab8a759f025a8e8
Elvin <ehuseyna@gmail.com>
2019-10-22 09:01:51 +0400

    --Alphabet

---
+++ homework1/src/main/java/Lesson4.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Lesson4 {
+    static String aZ(int n) {
+    String aZ = "ABCDEFGHIGKLMNOPQRSTUVWXYZ" + "abcdefghigklmnopqrstuvwxyz";
+    StringBuilder sB = new StringBuilder(n);
+    for (int i = 0; i < n; i++) {
+        int ZZ = (int) (aZ.length()* Math.random());
+        sB.append(aZ.charAt(ZZ));
+    }
+    return sB.toString();
+    }
+
+    public static void main(String[] args) {
+        int n = 30;
+        System.out.println(Lesson4.aZ(n));
+    }
+}
--- homework1/src/main/java/PublicclassEnvelope.java
+++ homework1/src/main/java/PublicclassEnvelope.java
@@ -5,8 +5,9 @@ public class PublicclassEnvelope {
         int height =8;
         double tan =(double) width/height;
         System.out.printf("Width: %d, Height: %d, tan: %.2f\n", width, height, tan);
-            for (int i = 0; i < width; i++) {
+            for (int x = 0; x < width; x++) {
                 System.out.printf("*");
+
 
 
             }

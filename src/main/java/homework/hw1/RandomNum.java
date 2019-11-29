af629499414df03762055b91d0b94b53d95adf15
Inara <inara.mustafayeva666@gmail.com>
2019-11-29 23:55:07 +0400

    doing hw4

--- src/main/java/homework/hw1/RandomNum.java
+++ src/main/java/homework/hw1/RandomNum.java
@@ -7,11 +7,20 @@ public class RandomNum {
     public static void main(String[] args) {
     Random random = new Random();
     int random_generated = random.nextInt(101);
-     Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: " );
+//        new Random().ints(0, 101);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! Enter your name: " );
         String name = scanner.nextLine();
-        System.out.println("Let the game begin!");
-    }
+        System.out.println("Let the game begin! Please insert your number: ");
+        int my_number;
+        do {
+            my_number = scanner.nextInt();
+            if (my_number < random_generated) {
+                System.out.println("Your number is too small. Please, try again...");
+            } else if (my_number > random_generated) {
+                System.out.println("Your number is too big. Please, try again...");
+            }
+        }            while (my_number != random_generated) ;
 
-}
+    }}
 

6b50791f4bb8cd321d034b3fd2ee374d5f31f8ae
Inara <failoglu96@gmail.com>
2019-11-30 14:08:26 +0400

    hw1 is done

--- src/main/java/homework/hw1/RandomNum.java
+++ src/main/java/homework/hw1/RandomNum.java
@@ -1,6 +1,4 @@
-package homework.hw1;
-
-import java.util.Random;
+package homework.hw1;import java.util.Random;
 import java.util.Scanner;
 public class RandomNum {
     public static void main(String[] args) {
@@ -14,13 +12,13 @@ public class RandomNum {
         int my_number;
         do {
             my_number = scanner.nextInt();
-            if (my_number < random_generated) {
+            if (my_number < random_generated){
                 System.out.println("Your number is too small. Please, try again...");
-            } else {
+            } else if (my_number > random_generated){
                 System.out.println("Your number is too big. Please, try again...");
             }
         }            while (my_number != random_generated);
-        System.out.printf("%s Win!",name);
-
-    }}
+        System.out.printf(" Congratulations %s, you won!",name);
+    }
+}
 

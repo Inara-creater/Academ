4f664d8e960bc183da76908d20b50e2f8fcbb712
Inara <inara.mustafayeva666@gmail.com>
2019-12-08 11:44:10 +0400

    doing hw2

--- src/main/java/homework/hw2/ShootingApp.java
+++ src/main/java/homework/hw2/ShootingApp.java
@@ -1,16 +1,13 @@
 package homework.hw2;
-
-import java.util.Random;
-import java.util.Scanner;
 public class ShootingApp {
     public static void main(String[] args) {
-        int[][] square = new int[6][6];
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("All set. Get ready to rumble!");
-        System.out.println("Please enter a line for fire...");
-        int player = scanner.nextInt();
-        int target = random.nextInt();
-
+        String [][] array = new String[6][6];
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                System.out.print("| - |");
+            }
+            System.out.println();
     }
-}
+        }
+
+            }
